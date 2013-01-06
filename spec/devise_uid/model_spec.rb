ec31035e0f514d591c2c4ca6ce6a8676c62c1781
Jingwen Owen Ou <jingweno@gmail.com>
2013-01-05 19:56:10 -0800

    Fix 1.8 compatibility

--- spec/devise_uid/model_spec.rb
+++ spec/devise_uid/model_spec.rb
@@ -6,15 +6,15 @@ end
 
 describe User do
   it "generates uid before save" do
-    user = User.create!(email: "foo@bar.com")
+    user = User.create!(:email => "foo@bar.com")
     user.uid.should be
   end
 
   it "doesn't generate uid if it has it" do
-    user = User.create!(email: "foo@bar.com")
+    user = User.create!(:email => "foo@bar.com")
     uid = user.uid
 
-    user.update_attributes(email: "bar@baz.com")
+    user.update_attributes(:email =>"bar@baz.com")
     user.uid.should == uid
   end
 end
