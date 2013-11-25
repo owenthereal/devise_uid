9b0fb5d4056c4bd9918efaf1fa7b9ac2f09b8260
Jingwen Owen Ou <jingweno@gmail.com>
2013-11-25 06:39:17 -0800

    Fix incorrect table name in generated migration

--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -1,6 +1,6 @@
 class DeviseUidAddTo<%= table_name.camelize %> < ActiveRecord::Migration
   def change
-    add_column :users, :uid, :string
-    add_index :users, :uid, :unique => true
+    add_column :<%= table_name %>, :uid, :string
+    add_index :<%= table_name %>, :uid, :unique => true
   end
 end
