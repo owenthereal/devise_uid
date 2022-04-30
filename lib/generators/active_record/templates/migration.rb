d3d92266b8234d00f92b2bfb4c0a12ab017fab10
Abrar Jahin <66951426+ajpalok@users.noreply.github.com>
2022-04-30 20:44:20 +0600

    Preventing Standard Error
    Directly inheriting from ActiveRecord::Migration is not supported, So it is required to specify that.

--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -1,4 +1,4 @@
-class DeviseUidAddTo<%= table_name.camelize %> < ActiveRecord::Migration
+class DeviseUidAddTo<%= table_name.camelize %> < ActiveRecord::Migration[7.0]
   def change
     add_column :<%= table_name %>, :uid, :string
     add_index :<%= table_name %>, :uid, :unique => true
