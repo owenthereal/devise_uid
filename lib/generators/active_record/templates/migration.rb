86ea43c2449428e2673cff76b0d28a3b860e558e
Abrar Jahin <66951426+ajpalok@users.noreply.github.com>
2022-05-05 14:21:04 +0600

    Preventing Active record's standard error
    Just a small integration of migration version in migration template

--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -1,4 +1,4 @@
-class DeviseUidAddTo<%= table_name.camelize %> < ActiveRecord::Migration[7.0]
+class DeviseUidAddTo<%= table_name.camelize %> < ActiveRecord::Migration<%= migration_version %>
   def change
     add_column :<%= table_name %>, :uid, :string
     add_index :<%= table_name %>, :uid, :unique => true
